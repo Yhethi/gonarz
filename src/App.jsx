c693cf0cc2bdc8301080b2c897095b1a3df56e8a
Yhethi <eloyjosebonillag@gmail.com>
2025-01-02 14:42:21 -0400

    Updated: Styles hero and token section

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <RoadmapSection />
       <PartnersSection />
       <TokenSection />
-      <FundHoldingsSection />
+      {/* <FundHoldingsSection /> */}
       <Luciernagas />
     </>
   );
