8003c99c6cd87d90df5a345a5485be46096b4cd3
Yhethi <eloyjosebonillag@gmail.com>
2025-01-01 23:29:42 -0400

    Updated: Left Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,16 @@ export const Hero = () => {
       <SquaresAnimation />
       <div className="hero__content">
         <div className="hero__left">
+          <div className="hero__text">
+            <h1>Discover the Future of Meme Investments</h1>
+            <h4>
+              A Meme Fund built to thrive on high-volatility opportunities.
+            </h4>
+            <h6>
+              At Meme Economy Fund, we focus on low to mid-cap assets with the
+              highest potential for exponential growth
+            </h6>
+          </div>
           <div className="center__hero__floor">
             <div className="hero__light__floor"></div>
           </div>
