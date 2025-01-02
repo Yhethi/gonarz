83d74ae949a3aae7788ede85d4fdf3bc2ff7884e
Yhethi <eloyjosebonillag@gmail.com>
2025-01-02 00:21:00 -0400

    Updated: Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/scss/hero.scss";
 import { SquaresAnimation } from "./utils/SquaresAnimation";
+import { useEffect } from "react";
+// PNG
+// import girl1 from "../assets/imgs/girl1.png";
+// import girl2 from "../assets/imgs/girl2.png";
+// import girl3 from "../assets/imgs/girl3.png";
+// JPG
+import girl1 from "../assets/imgs/girl1.jpg";
+import girl2 from "../assets/imgs/girl2.jpg";
+import girl3 from "../assets/imgs/girl3.jpg";
+
 export const Hero = () => {
+  const [girl, setGirl] = useState(girl1);
+
+  useEffect(() => {
+    let index = 1;
+    const changeGirlHandler = () => {
+      switch (index) {
+        case 1:
+          setGirl(girl1);
+          break;
+        case 2:
+          setGirl(girl2);
+          break;
+        case 3:
+          setGirl(girl3);
+          break;
+        default:
+          setGirl(girl1);
+          break;
+      }
+      index++;
+      if (index === 4) {
+        index = 0;
+      }
+    };
+    const interval = setInterval(changeGirlHandler, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div data-aos="fade-up" className="global__hero">
       <SquaresAnimation />
@@ -17,11 +55,17 @@ export const Hero = () => {
               highest potential for exponential growth
             </h6>
           </div>
-          <div className="center__hero__floor">
+          {/* <div className="center__hero__floor">
             <div className="hero__light__floor"></div>
+          </div> */}
+        </div>
+        <div className="hero__right">
+          <div className="girl__container">
+            <div className="girl__card">
+              <img src={girl} alt={girl} />
+            </div>
           </div>
         </div>
-        <div className="hero__right"></div>
       </div>
     </div>
   );
